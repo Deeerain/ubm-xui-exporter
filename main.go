cff187a3d26b13246f3b4200761ab1006fe424bf
Deerain <1deerain1@gmail.com>
2026-06-16 17:51:01 +0300

    init prometheus and server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/Deeerain/ubm-xui-exporter/config"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
 func main() {
-	println("Hello, World!")
+	config, err := config.ParseEnv()
+	if err != nil {
+		panic(err)
+	}
+	registry := prometheus.NewRegistry()
+	mux := http.NewServeMux()
+
+	mux.Handle(config.MetricsPath, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	http.ListenAndServe(config.ListenAddress, mux)
 }
