a14b85c30db5ca3130fe6b937cb311132faa6d32
Deerain <1deerain1@gmail.com>
2026-06-16 17:50:04 +0300

    add config values for server

--- config/config.go
+++ config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import "os"
+
+type Config struct {
+	ListenAddress string
+	MetricsPath   string
+}
+
+func ParseEnv() (*Config, error) {
+	config := Config{}
+
+	config.ListenAddress = parseEnvOrDefault("LISTEN_ADDRESS", ":8080")
+	config.MetricsPath = parseEnvOrDefault("METRICS_PATH", "/metrics")
+
+	return &config, nil
+}
+
+func parseEnvOrDefault(envKey, defaultValue string) string {
+	if value := os.Getenv(envKey); value != "" {
+		return value
+	}
+	return defaultValue
+}
