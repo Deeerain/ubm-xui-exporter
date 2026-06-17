0a3b2fb1f1934abe529a3f1cf9d32196a5ff5546
Deerain <1deerain1@gmail.com>
2026-06-17 03:05:10 +0300

    rewrite tests

--- api/test/handlers.go
+++ api/test/handlers.go
@@ -4,15 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/Deeerain/ubm-xui-exporter/api"
 )
 
 func OnlinesHandler(mockToken string, users ...string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var mockResponse struct {
-			Obj []string
+		mockResponse := api.ApiResponse[[]string]{
+			Obj: users,
 		}
-
-		mockResponse.Obj = []string(users)
 
 		if r.Header.Get("Authorization") != fmt.Sprintf("Bearer %s", mockToken) {
 			w.WriteHeader(http.StatusUnauthorized)
@@ -27,29 +27,14 @@ func OnlinesHandler(mockToken string, users ...string) http.HandlerFunc {
 
 func UniqueIpsHandler(mockToken string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		type IpInfo struct {
-			Ip string `json:"ip"`
-		}
-		type ClientInfo struct {
-			Id  int      `json:"id"`
-			Ips []IpInfo `json:"ips"`
-		}
-		var mockResponse struct {
-			Obj []ClientInfo `json:"obj"`
-		}
-
-		mockResponse.Obj = []ClientInfo{
-			{
-				Id: 1,
-				Ips: []IpInfo{
-					{Ip: "192.168.0.1"},
-					{Ip: "192.168.0.2"},
-				},
-			},
-			{
-				Id: 2,
-				Ips: []IpInfo{
-					{Ip: "192.168.0.1"},
+		mockResponse := api.ApiResponse[[]api.ClientIpInfo]{
+			Obj: []api.ClientIpInfo{
+				{
+					Id: 1,
+					Ips: []api.IPInfo{
+						{Ip: "192.168.0.1"},
+						{Ip: "192.168.0.2"},
+					},
 				},
 			},
 		}
